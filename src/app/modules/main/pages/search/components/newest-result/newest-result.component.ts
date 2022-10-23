d4472b06f9db397b31f748e4d5f72326374d8aff
mrymahmdlo <ahmdlomrym@gmail.com>
2022-10-23 15:50:32 +0330

    .

--- src/app/modules/main/pages/search/components/newest-result/newest-result.component.ts
+++ src/app/modules/main/pages/search/components/newest-result/newest-result.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {FlexModule} from '@angular/flex-layout';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { RouterModule } from '@angular/router';
-import { SearchLayoutComponent } from '../../pages';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FlexModule } from "@angular/flex-layout";
+import { NzTypographyModule } from "ng-zorro-antd/typography";
+import { RouterModule } from "@angular/router";
+import { SearchLayoutComponent } from "../../pages";
 @Component({
-  selector: 'app-newest-result',
+  selector: "app-newest-result",
   standalone: true,
-  imports: [CommonModule, FlexModule,SearchLayoutComponent,NzTypographyModule,RouterModule],
-  templateUrl: './newest-result.component.html',
-  styleUrls: ['./newest-result.component.less']
+  imports: [
+    CommonModule,
+    FlexModule,
+    SearchLayoutComponent,
+    NzTypographyModule,
+    RouterModule,
+  ],
+  templateUrl: "./newest-result.component.html",
+  styleUrls: ["./newest-result.component.less"],
 })
 export class NewestResultComponent implements OnInit {
+  constructor() {}
 
-
-  constructor() { }
-  
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }
