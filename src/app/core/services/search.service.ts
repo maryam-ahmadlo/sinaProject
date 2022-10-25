63800d17a170ba2fa621f3f6e7dc8d5d0f7a37bb
mrymahmdlo <ahmdlomrym@gmail.com>
2022-10-25 13:15:18 +0330

    simpleSearch

--- src/app/core/services/search.service.ts
+++ src/app/core/services/search.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SearchService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  simpleSearch(name: string) {
+    return this.httpClient.get("/api/search/findByName?name=", {
+      headers: new HttpHeaders({
+        accept: "application/json",
+        Authorization: "Basic b2ttQWRtaW46YWRtaW4=",
+      }),
+      params: { name: name },
+    });
+  }
 }
